e9ee7d12ea644673af146d66e70d2831fcbd4d32
kusupon <kusupon0628@gmail.com>
2025-03-18 23:38:02 +0900

    lambda test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource.js';
+import { text } from './text/resource.js';
 
 defineBackend({
   auth,
   data,
   storage,
+  text
 });
--- amplify/src/handlers/index.js
+++ amplify/src/handlers/index.js
@@ -1,28 +1,27 @@
 // src/handlers/index.js
 
-const textractService = require('../services/textractService');
-const comprehendService = require('../services/comprehendService'); // 追加：Comprehendサービスの読み込み
-const logger = require('../utils/logger');
+const textractService = require('../services/textractServices');
+const comprehendService = require('../services/comprehendService');
 
 exports.handler = async (event) => {
     try {
-        logger.info('Received event', JSON.stringify(event, null, 2));
+        console.info('Received event', JSON.stringify(event, null, 2));
 
         for (const record of event.Records) {
             const bucketName = record.s3.bucket.name;
             const objectKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
 
-            logger.info(`Processing file: ${objectKey} from bucket: ${bucketName}`);
+            console.info(`Processing file: ${objectKey} from bucket: ${bucketName}`);
 
             const textractResult = await textractService.extractText(bucketName, objectKey);
-            logger.info('Textract result:', JSON.stringify(textractResult, null, 2));
+            console.info('Textract result:', JSON.stringify(textractResult, null, 2));
 
             // 追加: 抽出されたテキストが存在する場合、Comprehendで解析を実行
             if (textractResult.text && textractResult.text.trim() !== '') {
                 const comprehendResult = await comprehendService.analyzeText(textractResult.text);
-                logger.info('Comprehend result:', JSON.stringify(comprehendResult, null, 2));
+                console.info('Comprehend result:', JSON.stringify(comprehendResult, null, 2));
             } else {
-                logger.warn('No text extracted to analyze with Comprehend.');
+                console.warn('No text extracted to analyze with Comprehend.');
             }
         }
 
@@ -31,7 +30,7 @@ exports.handler = async (event) => {
             body: JSON.stringify({ message: 'Processing completed successfully' })
         };
     } catch (error) {
-        logger.error('Error processing event:', error);
+        console.error('Error processing event:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({
